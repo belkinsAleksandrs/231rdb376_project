03a82de33f2078d065ea58c0900d85ee1398fcab
Aleksandrs Belkins <aleksandrs.belkins@gmail.com>
2023-12-22 13:25:05 +0200

    adding mail functionality

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from openpyxl import Workbook, load_workbook 
+import smtplib 
+from email.message import EmailMessage
 import time
 
 service = Service()
@@ -17,6 +19,7 @@ url = "https://ss.lv"
 delay = 1
 max_price = 80000
 number_of_rooms = "2"
+file_path = "apartments-list.xlsx"
 
 driver.get(url)
 time.sleep(delay)
@@ -42,7 +45,6 @@ find = driver.find_element(By.ID, "f_o_8_max").send_keys(max_price)
 find = driver.find_element(By.CLASS_NAME, "s12").click()
 time.sleep(delay)
 
-
 table = driver.find_element(By.XPATH, "//form/table[@align='center']")
 rows = table.find_elements(By.TAG_NAME, "tr")
 
@@ -54,6 +56,40 @@ for row in rows[1:-1]:
     table_data.append(row_data)
     ws.append(row_data)
 
-wb.save('apartments-list.xlsx')
+wb.save(file_path)
 wb.close()
 print(table_data)
+
+
+my_email = "dip225project@inbox.lv"
+my_password = ""
+
+inbox_server = "mail.inbox.lv"
+inbox_port = 587
+
+try:
+    my_server = smtplib.SMTP(inbox_server, inbox_port)
+    my_server.ehlo()
+    my_server.starttls()
+
+    my_server.login(my_email, my_password)
+
+    msg = EmailMessage()
+    msg.set_content('Apartments in Agenskalns:')
+
+    msg['Subject'] = 'Apartments'
+    msg['From'] = my_email
+    msg['To'] = my_email
+
+    with open(file_path, 'rb') as file:
+        file_data = file.read()
+        msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file.name)
+
+    my_server.send_message(msg)
+    print('Mail Sent')
+
+except Exception as e:
+    print(f'Error: {e}')
+
+finally:
+    my_server.quit()
